1036f6601f66b8b9f1370fa6528af64a8d7376cb
kalunik <nikitaex123@gmail.com>
2022-03-29 23:49:40 +0300

    module01/ex05 next todo

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -4,22 +4,51 @@
 
 #include <iostream>
 #include <fstream>
-#include <istream>
-#include <stdio.h>
 
 int	main(int ac, char **av){
 	if (ac != 4){
 		std::cout << "Need more arguments (give 3 args)" << std::endl;
 		return (EXIT_FAILURE);
 	}
-	std::ifstream	ifs(av[1]);
-	std::string		s1;
-	std::string		s2;
-	ifs>>s1>>s2;
-	std::cout << ifs.getline(s1, 10, "\n") << "|" << s2 << std::endl;
-//	ifs.close();
+	std::string		filename = av[1],
+					oldStr = av[2],
+					newStr = av[3];
 
+	std::ifstream	ifs(filename);
+	if (!ifs.is_open()){
+		std::cerr << "Couldn't open to read" << filename << std::endl;
+		return (EXIT_FAILURE);
+	}
 
+	std::ofstream	ofs(filename + ".replace");
+	if (!ofs.is_open()){
+		ifs.close();
+		std::cerr << "Couldn't open to write " << filename + ".replace" << std::endl;
+		return (EXIT_FAILURE);
+	}
 
+	std::string		txt;
+	for (std::string buf; std::getline(ifs, buf);){
+		txt += buf;
+		if (!ifs.eof())
+			txt += "\n";
+	}
+
+	std::string	tmp;
+	for (size_t pos; 1;){
+		pos = txt.find(oldStr);
+		if (pos != std::string::npos){
+			tmp = txt.substr(0, pos);
+			tmp += newStr;
+			tmp += txt.substr(pos + oldStr.length(), txt.length());
+			txt = tmp;
+		}
+		else
+			break;
+	}
+
+	ofs << txt;
+	ifs.close();
+	ofs.close();
 	return (EXIT_SUCCESS);
 }
