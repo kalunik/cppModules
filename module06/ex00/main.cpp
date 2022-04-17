70f68fb2225f82d0f5f155a8fedfd35553247c5b
kalunik <nikitaex123@gmail.com>
2022-04-17 11:39:18 +0300

    starting cpp06

---
+++ module06/ex00/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Waif Jonathon on 16.04.2022.
+//
+
+#include "iostream"
+
+int main(int ac, char **av) {
+	if (ac != 2) {
+		std::cerr << "It works only with 1 argument." << std::endl;
+		return (EXIT_FAILURE);
+	}
+
+}
